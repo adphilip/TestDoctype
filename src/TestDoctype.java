2da2b729750f499ca6ef73657f8d7e27e40b1713
Adrian Filipescu <adrian.filipescu@gmail.com>
2015-04-23 18:20:50 +0300

    Initial version

---
+++ src/TestDoctype.java
@@ -0,0 +1,33 @@
+/**
+ * @category Test
+ * @author afilipescu
+ * What should we do here, Mr. xxxx?
+ */
+public class TestDoctype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/**
+		 * @description Call the functions
+		 */
+		int c = sumAB(3,4);
+		System.out.println("Sum : " + c);
+	}
+	
+	/**
+	 * @see 
+	 * It's a very complicated function :). See bellow!
+	 * x = a *45 / 45
+	 * @param a - int, b - int
+	 * @return Sum of two numbers
+	 * 
+	 */
+	private static int sumAB(int a, int b){
+		/**
+		 * A lot of code!!!!!
+		 */
+		return a+b;
+	}
+	
+}
